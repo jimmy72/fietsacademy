8217d9bd373e8e8c54a6d349424c22d579bb9621
Jimmy <Jimmy@PC-JIMMY.home>
2018-09-24 14:04:30 +0200

    last commit

---
+++ src/main/java/be/vdab/fietsacademy/entities/Docent.java
@@ -0,0 +1,72 @@
+package be.vdab.fietsacademy.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import be.vdab.fietsacademy.enums.Geslacht;
+
+@Entity
+@Table(name = "docenten")
+public class Docent implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	private long id;
+	private String voornaam;
+	private String familienaam;
+	private BigDecimal wedde;
+	private String emailAdres;
+	@Enumerated(EnumType.STRING)
+	private Geslacht geslacht;
+	
+	protected Docent() {
+		//Je maakt de default constructor protected in plaats van public als je liever hebt dat zo weinig mogelijk
+		//classes deze constructor kunnen gebruiken
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getVoornaam() {
+		return voornaam;
+	}
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+	public String getFamilienaam() {
+		return familienaam;
+	}
+	public void setFamilienaam(String familienaam) {
+		this.familienaam = familienaam;
+	}
+	public BigDecimal getWedde() {
+		return wedde;
+	}
+	public void setWedde(BigDecimal wedde) {
+		this.wedde = wedde;
+	}
+	public String getEmailAdres() {
+		return emailAdres;
+	}
+	public void setEmailAdres(String emailAdres) {
+		this.emailAdres = emailAdres;
+	}
+
+	public Geslacht getGeslacht() {
+		return geslacht;
+	}
+
+	public void setGeslacht(Geslacht geslacht) {
+		this.geslacht = geslacht;
+	}
+	
+}
---
+++ src/test/java/be/vdab/fietsacademy/repositories/JpaDocentRepositoryTest.java
@@ -0,0 +1,59 @@
+package be.vdab.fietsacademy.repositories;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import be.vdab.fietsacademy.entities.Docent;
+import be.vdab.fietsacademy.enums.Geslacht;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Sql("/insertDocent.sql")
+@Import(JpaDocentRepository.class)
+public class JpaDocentRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+	@Autowired
+	private JpaDocentRepository repository;
+	
+	private long idVanTestMan() {
+		return super.jdbcTemplate.queryForObject("select id from docenten where voornaam = 'testM'", Long.class);
+	}
+	
+	private long idVanTestVrouw() {
+		return super.jdbcTemplate.queryForObject("select id from docenten where voornaam = 'testV'", Long.class);
+	}
+	@Test
+	public void read() {
+		Docent docent = repository.read(idVanTestMan()).get();
+		assertEquals("testM", docent.getVoornaam());
+		docent = repository.read(idVanTestVrouw()).get();
+		assertEquals("testV", docent.getVoornaam());
+	}
+	@Test
+	public void readOnbestaandeDocent() {
+		assertFalse(repository.read(-1).isPresent());
+	}
+	
+	@Test
+	public void man() {
+		assertEquals(Geslacht.MAN, repository.read(idVanTestMan()).get().getGeslacht());
+	}
+	
+	@Test
+	public void vrouw() {
+		assertEquals(Geslacht.VROUW, repository.read(idVanTestVrouw()).get().getGeslacht());
+	}
+
+}
