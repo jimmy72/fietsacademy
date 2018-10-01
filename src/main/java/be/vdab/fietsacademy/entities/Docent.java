dede3b79f9d7c46019cd428791ae309d31dc6629
Jimmy <Jimmy@PC-JIMMY.home>
2018-10-01 14:54:55 +0200

    test

--- src/main/java/be/vdab/fietsacademy/entities/Docent.java
+++ src/main/java/be/vdab/fietsacademy/entities/Docent.java
@@ -47,7 +47,7 @@ public class Docent implements Serializable{
 	
 	protected Docent() {
 		//Je maakt de default constructor protected in plaats van public als je liever hebt dat zo weinig mogelijk
-		//classes deze constructor kunnen gebruiken
+		//classes deze constructor kunnen gebruiken.
 	}
 	
 	public Docent(String voornaam, String familienaam, BigDecimal wedde, String emailAdres, Geslacht geslacht, Campus campus) {
